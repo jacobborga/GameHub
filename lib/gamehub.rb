04d41f7db1f7c1904fd34ee6cc77adb134578a68
jacobborga <jdborga@gmail.com>
2021-03-05 10:19:34 -0700

    ability to deposit cash for tokens at a 10x rate

--- lib/gamehub.rb
+++ lib/gamehub.rb
@@ -8,6 +8,16 @@ class GameHub
         @player_name = gets.chomp
         puts "Welcome #{@player_name}!"
     end
+
+    def deposit_cash
+        puts "Please enter a cash $ amount you would like to exchange for tokens..."
+        @usd_balance = gets.chomp 
+        exchange_rate
+    end
+
+    def exchange_rate
+        @balance = @usd_balance * 10
+    end
 end
 
 game = GameHub.new
