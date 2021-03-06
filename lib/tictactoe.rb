360fc0c5a2e5fdb0269f090d7e813cd557f66f28
jacobborga <jdborga@gmail.com>
2021-03-05 18:00:24 -0700

    Added some logic to tictactoe

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,5 +1,8 @@
+CLEAR_SCREEN = "\e[H\e[2J"
+
 class TicTacToe 
     def initialize(bet)
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         @balance = bet 
         play
     end
@@ -7,5 +10,26 @@ class TicTacToe
     def play
         puts "Balance: #{@balance}"
         puts "Welcome to Tic-Tac-Toe!"
+        display_board
+    end
+
+    def display_board
+        row = "-----------"
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts row
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts row 
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def position_taken?(index)
+        if board[index] == "X" || board[index] == "O"
+            return true
+        end
+        return false
+    end
+
+    def move(index, player)
+
     end
 end
